0044256f25a5387253a7559a199988a32221a96f
reid <reidsherman@gmail.com>
2017-09-01 19:10:24 -0700

    updated projects page

---
+++ assets/js/projects.js
@@ -0,0 +1,8 @@
+// Doesn't quite work when more than one Read More are open simultaenously
+
+$(".readmore").click(function(){
+  $(this).addClass('hide');
+});
+$(".readless").click(function(){
+  $(".readmore").removeClass('hide');
+});
