0bd18472daf59e3384d7e149612d27dd11b67b64
reid <reidsherman@gmail.com>
2017-09-01 19:10:47 -0700

    updated projects page

--- projects.php
+++ projects.php
@@ -1,62 +1,73 @@
 <?php include("partials/header.html"); ?>
-  <!-- Page under construction! (obviously) -->
+  <link rel="stylesheet" href="assets/styles/projectstyle.css">
     <div class="container">
       <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-6 pink">
-          <div class="card" style="width: 20rem;">
-            <img class="card-img-top" src="..." alt="iOS Run Grouper">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 pink">
+          <div class="card">
+            <img class="card-img-top" src="assets/img/running.jpeg" alt="iOS Run Grouper">
             <div class="card-block">
               <h4 class="card-title">Run Grouper iOS App</h4>
-              <p class="card-text">Are you interested in running, but don't like running alone? The Run Group Tracker iOS App
-              loads your run groups' routes, times, and dates, then distributes it as a list to all active members in the group with the app.
-              </p>
-              <a href="#" data-toggle="collapse" data-target="#demo">Read more...</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 pink">
-          <div class="card" style="width: 20rem;">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-block">
-              <h4 class="card-title">Reddit Scraper Bot</h4>
-              <p class="card-text">It's such a drag to have to open a new window and check if a movie or TV show is on Netflix Streaming,
-              so I made a bot to automatically tell you! It relies on the NetflixRoulette and praw APIs to check if the title is
-              currently available.
-              <a data-toggle="collapse" data-target="#demo2">Read more...</a>
-             </p>
-             <p class="collapse" id="demo2">
+              <p class="card-text">Like running, but don't like running alone? Run Grouper iOS App
+              loads your run groups' routes, times, and dates, then distributes it as a list to all active members who have the app.
 
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-               <a href="#" data-toggle="collapse" data-target="#demo2">Hide</a>
-             </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 pink">
-          <div class="card" style="width: 20rem;">
-            <img class="card-img-top" src="..." alt="Card image cap">
-            <div class="card-block">
-              <h4 class="card-title">Earth Science Resources</h4>
-              <p class="card-text">Are you an Earth Science Teacher looking for teaching resources, or a student studying for the
-                New York Regents Exam? Check out this repository of Earth Science Resources I've created/collected over the years.
-                <a href="#" data-toggle="collapse" data-target="#demo3">Read more...</a>
-              </p>
-              <p class="collapse" id="demo3">
+              <a class="readmore" data-toggle="collapse" data-target="#demo">Read more...</a>
+              <span class="collapse" id="demo">
 
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit,
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-
+                <a class="readless" data-toggle="collapse" data-target="#demo">Hide</a>
+              </span>
               </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 pink">
+          <div class="card">
+            <img class="card-img-top" src="assets/img/movie.jpeg" alt="Reddit Scraper">
+            <div class="card-block">
+              <h4 class="card-title">Reddit Scraper Bot</h4>
+              <p class="card-text">It's such a drag to have to open a new window and check if a movie or TV show is on Netflix Streaming,
+              so this bot will magically tell you! It uses the NetflixRoulette and praw APIs to crawl through reddit for content titles,
+              then replies when it finds one.
+              <a class="readmore" data-toggle="collapse" data-target="#demo2">Read more...</a>
 
+             <span class="collapse" id="demo2">
+
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+               <a class="readless" data-toggle="collapse" data-target="#demo2">Hide</a>
+             </span>
+             </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 pink">
+          <div class="card">
+            <img class="card-img-top" src="assets/img/earth.jpeg" alt="ES Resources">
+            <div class="card-block">
+              <h4 class="card-title">Earth Science Resources</h4>
+              <p class="card-text">Are you an Earth Science Teacher looking for teaching resources, or a student studying for the
+                New York Regents Exam? Check out this repository of Earth Science Resources I've created and/or collected over the years.
+                <a class="readmore" data-toggle="collapse" data-target="#demo3">Read more...</a>
+
+              <span class="collapse" id="demo3">
+
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                <a class="readless" data-toggle="collapse" data-target="#demo3">Hide</a>
+              </span>
+              </p>
             </div>
           </div>
         </div>
       </div>
 
     </div>
+    <script type="text/javascript" src="assets/js/projects.js">
 
+    </script>
   </body>
 </html>
