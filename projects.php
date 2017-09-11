c9d1f123dcadff70e32adc7db1bca3677b7ab0f9
reid <reidsherman@gmail.com>
2017-09-11 14:53:58 -0700

    removed readmore and readless stuff from projects page, added links to projects instead

--- projects.php
+++ projects.php
@@ -9,15 +9,7 @@
               <h4 class="card-title">Run Grouper iOS App</h4>
               <p class="card-text">Like running, but don't like running alone? Run Grouper iOS App
               loads your run groups' routes, times, and dates, then distributes it as a list to all active members who have the app.
-
-              <a class="readmore" data-toggle="collapse" data-target="#demo">Read more...</a>
-              <span class="collapse" id="demo">
-
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                <a class="readless" data-toggle="collapse" data-target="#demo">Hide</a>
-              </span>
+              Write-up with code coming soon.
               </p>
             </div>
           </div>
@@ -29,36 +21,20 @@
               <h4 class="card-title">Reddit Scraper Bot</h4>
               <p class="card-text">It's such a drag to have to open a new window and check if a movie or TV show is on Netflix Streaming,
               so this bot will magically tell you! It uses the NetflixRoulette and praw APIs to crawl through reddit for content titles,
-              then replies when it finds one.
-              <a class="readmore" data-toggle="collapse" data-target="#demo2">Read more...</a>
+              then replies when it finds one. Write-up with code coming soon.
 
-             <span class="collapse" id="demo2">
-
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-               <a class="readless" data-toggle="collapse" data-target="#demo2">Hide</a>
-             </span>
              </p>
             </div>
           </div>
         </div>
         <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 pink">
           <div class="card">
-            <img class="card-img-top" src="assets/img/earth.jpeg" alt="ES Resources">
+            <a href="https://reidsherman.com/esfiles.html"><img class="card-img-top" src="assets/img/earth.jpeg" alt="ES Resources"></a>
             <div class="card-block">
               <h4 class="card-title">Earth Science Resources</h4>
               <p class="card-text">Are you an Earth Science Teacher looking for teaching resources, or a student studying for the
                 New York Regents Exam? Check out this repository of Earth Science Resources I've created and/or collected over the years.
-                <a class="readmore" data-toggle="collapse" data-target="#demo3">Read more...</a>
-
-              <span class="collapse" id="demo3">
-
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                <a class="readless" data-toggle="collapse" data-target="#demo3">Hide</a>
-              </span>
+                <a href="https://reidsherman.com/esfiles.html">Earth Science Resources</a>
               </p>
             </div>
           </div>
