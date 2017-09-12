e96fd11a3f113007a332a7b9fed41ccf3f304962
reid <reidsherman@gmail.com>
2017-09-11 17:04:31 -0700

    updated project page with 8 projects

--- projects.php
+++ projects.php
@@ -2,45 +2,34 @@
   <link rel="stylesheet" href="assets/styles/projectstyle.css">
     <div class="container">
       <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 pink">
-          <div class="card">
-            <img class="card-img-top" src="assets/img/running.jpeg" alt="iOS Run Grouper">
-            <div class="card-block">
-              <h4 class="card-title">Run Grouper iOS App</h4>
-              <p class="card-text">Like running, but don't like running alone? Run Grouper iOS App
-              loads your run groups' routes, times, and dates, then distributes it as a list to all active members who have the app.
-              Write-up with code coming soon.
-              </p>
-            </div>
-          </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <?php include("partials/project_rungrouper.html"); ?>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 pink">
-          <div class="card">
-            <img class="card-img-top" src="assets/img/movie.jpeg" alt="Reddit Scraper">
-            <div class="card-block">
-              <h4 class="card-title">Reddit Scraper Bot</h4>
-              <p class="card-text">It's such a drag to have to open a new window and check if a movie or TV show is on Netflix Streaming,
-              so this bot will magically tell you! It uses the NetflixRoulette and praw APIs to crawl through reddit for content titles,
-              then replies when it finds one. Write-up with code coming soon.
-
-             </p>
-            </div>
-          </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <?php include("partials/project_redditscraper.html"); ?>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 pink">
-          <div class="card">
-            <a href="https://reidsherman.com/esfiles.html"><img class="card-img-top" src="assets/img/earth.jpeg" alt="ES Resources"></a>
-            <div class="card-block">
-              <h4 class="card-title">Earth Science Resources</h4>
-              <p class="card-text">Are you an Earth Science Teacher looking for teaching resources, or a student studying for the
-                New York Regents Exam? Check out this repository of Earth Science Resources I've created and/or collected over the years.
-                <a href="https://reidsherman.com/esfiles.html">Earth Science Resources</a>
-              </p>
-            </div>
-          </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <?php include("partials/project_esfiles.html"); ?>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <?php include("partials/project_circumbinarypaper.html"); ?>
         </div>
       </div>
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <?php include("partials/project_mastersthesis.html"); ?>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <?php include("partials/project_undergradthesis.html"); ?>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <?php include("partials/project_geologymap.html"); ?>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <?php include("partials/project_ctscan.html"); ?>
+        </div>
 
+      </div>
     </div>
     <script type="text/javascript" src="assets/js/projects.js">
 
